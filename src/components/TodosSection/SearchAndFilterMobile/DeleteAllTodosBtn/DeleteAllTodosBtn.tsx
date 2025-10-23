7ed11f9c6ab03dfec139e27f8631254a9029000e
Gagun45 <selyanchyn45@gmail.com>
2025-10-23 23:59:11 +0300

    alert dialog for removing all todos added

--- src/components/TodosSection/SearchAndFilterMobile/DeleteAllTodosBtn/DeleteAllTodosBtn.tsx
+++ src/components/TodosSection/SearchAndFilterMobile/DeleteAllTodosBtn/DeleteAllTodosBtn.tsx
@@ -1,6 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { clearTodos } from "@/redux/slices/todosSlice";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const DeleteAllTodosBtn = () => {
   const { todos } = useAppSelector((s) => s.todos);
@@ -10,9 +21,32 @@ const DeleteAllTodosBtn = () => {
   };
   if (todos.length === 0) return null;
   return (
-    <Button variant={'ghost'} onClick={onClearTodos}>
-      Delete all todos
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Delete all todos</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete all
+            todos.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="justify-between!">
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className={buttonVariants({ variant: "destructive" })}
+            onClick={onClearTodos}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    // <Button variant={'ghost'} onClick={onClearTodos}>
+    //   Delete all todos
+    // </Button>
   );
 };
 export default DeleteAllTodosBtn;
